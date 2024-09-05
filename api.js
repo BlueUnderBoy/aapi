b8ba4d4f416131892613de6eece7899e6409b056
BlueUnderBoy <lital5@yahoo.com>
2024-09-05 07:21:01 -0500

    mettre a jour

--- api.js
+++ api.js
@@ -3,12 +3,17 @@ const cfighter = document.getElementById('cfighter');
 const dog = document.getElementById('dog');
 const cat = document.getElementById('cat');
 
-dfighter.addEventListener("click", getNewDog);
-cfighter.addEventListener("click", getNewCat);
+dog.addEventListener("click", gnd);
+cat.addEventListener("click", gnc);
 
 const body = document.getElementById("cont");
 
+body.style.setProperty("height", "1000px")
+body.style.setProperty("width", "fill")
 body.style.setProperty("display", "flex")
+body.style.setProperty("justify-content", "center")
+body.style.setProperty("align-items", "center")
+body.style.setProperty("border-style", "solid")
 
 async function getNewDog() {
     dog.style.cursor = 'wait';
@@ -22,12 +27,21 @@ async function getNewDog() {
 
 async function getNewCat() {
     cat.style.cursor = 'wait';
-    const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=1");
     const jsonData = await response.json();
     const url = jsonData.message;
 
-    cfighter.src = url;
+    cfighter.src = jsonData[0]["url"];
+    console.log(jsonData)
     cat.style.cursor = 'pointer';
+}
+
+function gnd() {
+    getNewDog();
+}
+
+function gnc() {
+    getNewCat();
 }
 
 getNewDog();
